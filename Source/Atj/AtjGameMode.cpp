c71124af9acb5181dc2611bd5c053b1a1a5bbdf5
Tucker Paxton <tuckerpower@gmail.com>
2020-11-22 12:58:40 -0600

    Player can now pickup/putdown items with the 'p' button

--- Source/Atj/AtjGameMode.cpp
+++ Source/Atj/AtjGameMode.cpp
@@ -11,7 +11,7 @@ AAtjGameMode::AAtjGameMode()
 	PlayerControllerClass = AAtjPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/NpcCharacters/NpcCharacterBp"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
